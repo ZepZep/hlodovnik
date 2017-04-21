9663ee275736b2316429091ce3de4ebb7c4bbb3e
Petr Zelina <zelina.pe@gmail.com>
2017-04-21 22:56:15 +0200

    uvozovky, zavorky, lidi, vypustky

--- VG/raw/refine.py
+++ VG/raw/refine.py
@@ -1,10 +1,48 @@
 def putSection(s):
-    return "\\section{"+s+"}\n"
+    return "\\section*{"+s+"}\n"
 
 def putSubsection(s):
-    return "  \\subsection{"+s+"}\n"
+    return "  \\subsection*{"+s+"}\n"
+
+
+def zavorky(s):
+    s = s.replace("(", "(\\textit{")
+    s = s.replace(")", "})")
+    return s
+
+def vypustka(s):
+    return s.replace("...", "\\ldots{}")
+
+def uvozovky(s):
+    s = s.replace("„", "\"")
+    s = s.replace("“", "\"")
+    l = s.split("\"")
+    fin = s
+    if len(l)%2 == 0:
+        print("spatne uvozovky:")
+        print(s)
+    else:
+        fin = ""
+        for i, part in enumerate(l[0:-1]):
+            fin += part
+            if i % 2:
+                fin += "}"
+            else:
+                fin += "\\uv{"
+        fin += l[-1]
+    return fin
+
+def kurzJmena(s):
+    l = s.split(":")
+    fin = s
+    if len(l)>1 and len(l[0].split(" ")) < 3:
+        fin = "\\textit{" + l[0] + "}:" + ":".join(l[1:])
+    return fin
 
 def makeTexHlod(s):
+    s = zavorky(s)
+    s = uvozovky(s)
+    s = vypustka(s)
     return "    \\hlod{"+s+"}\n"
 
 with open('hlody.txt', 'r') as inFile:
@@ -24,16 +62,16 @@ with open('hlody.txt', 'r') as inFile:
                 l = line.split("\t")
                 if l[0] != rok:
                     rok = l[0]
-                    autor = l[1]
+                    autor = l[1].replace("-", "--")
                     outFile.write(putSection(rok))
                     outFile.write(putSubsection(autor))
-                elif l[1] != autor:
-                    autor = l[1]
+                elif l[1].replace("-", "--") != autor:
+                    autor = l[1].replace("-", "--")
                     outFile.write(putSubsection(autor))
                 
-                hlod += l[2][0:-1] + "\\newline\n"
+                hlod += kurzJmena(l[2][0:-1])
                 nextNewHlod = False
                 continue
             else:
-                hlod += line[0:-1] + "\\newline\n"
+                hlod += "\\newline\n    " + kurzJmena(line[0:-1]) 
          
